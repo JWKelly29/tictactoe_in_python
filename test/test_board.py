f08015a9ba0627a35fc4b5426672e6f49cc6a3f9
DarkChocLatte <darkchoclatte@gmail.com>
2017-02-19 16:22:49 +0000

    Created files: board.py and test_board.py. Test created: tests board initializes with an empty board. Method created: created gameBoard to return and empty board

---
+++ test/test_board.py
@@ -0,0 +1,14 @@
+import unittest
+from app.board import Board
+
+class TestBoard(unittest.TestCase):
+
+    def setUp(self):
+        self.board = Board()
+
+    def test_board_initailizes_with_empty_board(self):
+        empty_board = {1:' ' , 2: ' ', 3: ' ', 4:' ',  5:' ',  6:' ',  7:' ',  8:' ',  9:' '}
+        self.assertEqual(self.board.gameBoard(), empty_board)
+
+if __name__ == '__main__':
+    unittest.main()
