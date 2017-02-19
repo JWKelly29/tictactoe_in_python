bb7b6200c16579826658df4075657a3c75a5676c
DarkChocLatte <darkchoclatte@gmail.com>
2017-02-19 19:59:11 +0000

    files modified: game.py, test_game.py. Changed games initialize method to set player1 and player2 as instances of the player class

--- app/game.py
+++ app/game.py
@@ -1,10 +1,9 @@
 from app.board import Board
+from app.player import Player
 
 class Game():
     def __init__(self):
-        self.player1      = 'O'
-        self.player2      = 'X'
+        self.player1      = Player()
+        self.player2      = Player()
         self.boardControl = Board()
         self.gameBoard    = self.boardControl.gameBoard()
-
-    
--- test/test_game.py
+++ test/test_game.py
@@ -1,5 +1,6 @@
 import unittest
 from app.game import Game
+from app.player import Player
 
 class TestBoard(unittest.TestCase):
 
@@ -7,8 +8,6 @@ class TestBoard(unittest.TestCase):
         self.game = Game()
 
     def test_game_initializes_with_correct_properties(self):
-        self.assertEqual(self.game.player1, 'O')
-        self.assertEqual(self.game.player2, 'X')
+        self.assertIsInstance(self.game.player1, Player)
+        self.assertIsInstance(self.game.player2, Player)
         self.assertEqual(self.game.gameBoard, {1:' ' , 2: ' ', 3: ' ', 4:' ',  5:' ',  6:' ',  7:' ',  8:' ',  9:' '})
-
-    
