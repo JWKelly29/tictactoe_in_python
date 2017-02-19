d6a1ea44f575c839c73d8da0209227c0e09ea81f
DarkChocLatte <darkchoclatte@gmail.com>
2017-02-19 23:10:10 +0000

    files modified: test_game.py, game.py. Tested that game can check for a winner. Created method in game to check for a winner. Created a method in game to take input from user and give it to the setMarkBoard method for Board().

--- app/game.py
+++ app/game.py
@@ -17,3 +17,23 @@ class Game():
         player2_name = input(" : ")
         self.player2.setName(player2_name)
         print("Here is the board. Each cell of the grid is represented by the numbers 1 - 9 going from left to right, top to bottom.")
+
+
+    def takeTurn(self, mark):
+        print("Choose a place to put your mark")
+        position = int(input)
+        try:
+            if position > 9 or position < 1:
+                raise Exception
+        except:
+            print('Pick a number between 1-9')
+            return self.takeTurn(mark)
+
+        self.boardControl.setMarkBoard(mark, position)
+        self.boardControl.printBoard()
+
+    def is_game_won(self):
+        win_conds = ((1,2,3), (4,5,6), (7,8,9), (1,4,7), (2,5,8), (3,6,9), (1,5,9), (3,5,7))
+        for win_cond in win_conds:
+            if self.gameBoard[win_cond[0]] == self.gameBoard[win_cond[1]] and self.gameBoard[win_cond[1]] == self.gameBoard[win_cond[2]] and self.gameBoard[win_cond[0]]!= ' ':
+                return True
--- test/test_game.py
+++ test/test_game.py
@@ -2,7 +2,7 @@ import unittest
 from app.game import Game
 from app.player import Player
 
-class TestBoard(unittest.TestCase):
+class TestGame(unittest.TestCase):
 
     def setUp(self):
         self.game = Game()
@@ -11,3 +11,9 @@ class TestBoard(unittest.TestCase):
         self.assertIsInstance(self.game.player1, Player)
         self.assertIsInstance(self.game.player2, Player)
         self.assertEqual(self.game.gameBoard, {1:' ' , 2: ' ', 3: ' ', 4:' ',  5:' ',  6:' ',  7:' ',  8:' ',  9:' '})
+
+    def test_game_can_find_winner(self):
+        self.game.boardControl.setMarkBoard("X",1)
+        self.game.boardControl.setMarkBoard("X",2)
+        self.game.boardControl.setMarkBoard("X",3)
+        self.assertEqual(self.game.is_game_won(), True)
